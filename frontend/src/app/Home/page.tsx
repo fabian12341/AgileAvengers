ebdbbc4c22dc5c487bdb56f234cf9ecbb80c29c1
WarriorMind04 <josemiguel.guerrerojim@gmail.com>
2025-04-09 10:55:24 -0600

    Creación de dashboard funcional y responsivo

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -1,8 +1,10 @@
+// pages/Home.tsx
+"use client";
 import React from "react";
 import Navigation from "../components/Navigation";
 import Dashboard from "../components/Dashboard";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
     <div className="bg-gray-950 text-white min-h-screen p-6">
       <Navigation />
