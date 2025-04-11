3d0a58a863e728870e4ffe2399ea2af42df95b85
A01369765@tec.mx <you@example.com>
2025-04-11 12:41:57 -0600

    cambios

--- backend-python/app/__init__.py
+++ backend-python/app/__init__.py
@@ -8,12 +8,16 @@ from .routes import main
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path)
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)
 
+    if testing:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        app.config["TESTING"] = True
+    else:
+        app.config.from_object(Config)
+
+    db.init_app(app)
     app.register_blueprint(main)
 
     return app
-
