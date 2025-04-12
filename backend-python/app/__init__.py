cb7730ce97579b73c19af110a3ed45f4d0c8c5bb
A01369765@tec.mx <you@example.com>
2025-04-11 21:31:23 -0600

    conexión con oracle

--- backend-python/app/__init__.py
+++ backend-python/app/__init__.py
@@ -11,7 +11,7 @@ load_dotenv(dotenv_path)
 def create_app(testing=False):
     app = Flask(__name__)
     
-    CORS(app, supports_credentials=True)
+    CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
 
     if testing:
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
