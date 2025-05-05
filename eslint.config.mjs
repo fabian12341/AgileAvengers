dc1bd817677975013ae401fa5ae3f55f03f18eef
WarriorMind04 <josemiguel.guerrerojim@gmail.com>
2025-05-04 19:31:55 -0600

    Correción del lint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    // Extensiones recomendadas
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+      react: pluginReact,
+      "@typescript-eslint": tseslint.plugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+
+
+]);
