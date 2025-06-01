56129b83cdbc971cfcdd040c4f13439e789745c9
A01369765@tec.mx <you@example.com>
2025-06-01 16:34:33 -0600

    arreglar cors

--- backend-python/server.py
+++ backend-python/server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from app import create_app, db
 from app.models import User, Call, Transcript
 app = create_app()
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://agileavengers-2.onrender.com"]}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://agileavengers-2.onrender.com"]}}, supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
